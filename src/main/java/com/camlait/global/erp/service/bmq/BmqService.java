8c165ef831db288567c9af1e6ddfc052ea5939d8
Martin Blaise <mbsigne@gmail.com>
2015-12-28 13:37:50 -0500

    Add lambda expression for for loop statements

--- src/main/java/com/camlait/global/erp/service/bmq/BmqService.java
+++ src/main/java/com/camlait/global/erp/service/bmq/BmqService.java
@@ -20,279 +20,279 @@ import com.camlait.global.erp.domain.bmq.LigneBmq;
 import com.camlait.global.erp.domain.bmq.LigneBmqTaxe;
 import com.camlait.global.erp.domain.config.GlobalAppConstants;
 import com.camlait.global.erp.domain.document.Document;
-import com.camlait.global.erp.domain.document.LigneDeDocument;
 import com.camlait.global.erp.domain.document.LigneDeDocumentTaxe;
+import com.camlait.global.erp.domain.enumeration.TypeDocuments;
 import com.camlait.global.erp.domain.operation.Recouvrement;
 import com.camlait.global.erp.service.GlobalErpServiceException;
 
 public class BmqService implements IBmqService {
-    
-    @Autowired
-    private BmqDao bmqDao;
-    
-    @Autowired
-    private LigneBmqDao ligneBmqDao;
-    
-    @Autowired
-    private RecouvrementDao recouvrementDao;
-    
-    @Autowired
-    private LigneBmqTaxeDao ligneBmqTaxeDao;
-    
-    @Transactional
-    @Override
-    public Bmq ajouterBmq(Bmq bmq) {
-        
-        if (bmq != null) {
-            bmqDao.save(bmq);
-        }
-        return bmq;
-    }
-    
-    @Transactional
-    @Override
-    public Bmq modifierBmq(Bmq bmq) {
-        bmq.setDerniereMiseAJour(new Date());
-        bmqDao.saveAndFlush(bmq);
-        return bmq;
-    }
-    
-    @Override
-    public Bmq trouverBmq(Long bmqId) {
-        if (bmqId == null) {
-            throw new IllegalArgumentException("bmqId ne doit pas etre null");
-        }
-        final Bmq b = bmqDao.findOne(bmqId);
-        if (b != null) {
-            Hibernate.initialize(b.getLigneBmqs());
-            Hibernate.initialize(b.getDocuments());
-            Hibernate.initialize(b.getRecouvrements());
-            return b;
-        } else {
-            throw new GlobalErpServiceException(GlobalAppConstants.buildNotFingMessage(Bmq.class, bmqId));
-        }
-    }
-    
-    @Transactional
-    @Override
-    public void supprimerBmq(Long bmqId) {
-        bmqDao.delete(trouverBmq(bmqId));
-    }
-    
-    @Override
-    public Page<Bmq> listerBmq(Pageable p) {
-        return bmqDao.findAll(p);
-    }
-    
-    @Override
-    public Page<Bmq> listerBmq(Long vendeurId, Pageable p) {
-        return bmqDao.listerBmq(vendeurId, p);
-    }
-    
-    @Override
-    public Page<Bmq> listerBmq(Date debut, Date fin, Pageable p) {
-        return bmqDao.listerBmq(debut, fin, p);
-    }
-    
-    @Override
-    public Page<Bmq> listerBmq(Long vendeurId, Date debut, Date fin, Pageable p) {
-        return bmqDao.listerBmq(vendeurId, debut, fin, p);
-    }
-    
-    @Transactional
-    @Override
-    public Collection<LigneBmq> ajouterLigneBmq(Collection<LigneBmq> ligneBmqs) {
-        if (ligneBmqs != null) {
-            ligneBmqDao.save(ligneBmqs);
-            ajouterLigneBmqTaxe(ligneBmqs);
-        }
-        return ligneBmqs;
-    }
-    
-    @Override
-    public LigneBmq trouverLigneBmq(Long ligneBmqId) {
-        if (ligneBmqId == null) {
-            throw new IllegalArgumentException("ligneBmqId ne doit pas etre null");
-        }
-        final LigneBmq lb = ligneBmqDao.findOne(ligneBmqId);
-        if (lb != null) {
-            lb.getLigneBmqTaxes();
-            return lb;
-        } else {
-            throw new GlobalErpServiceException(GlobalAppConstants.buildNotFingMessage(LigneBmq.class, ligneBmqId));
-        }
-    }
-    
-    @Transactional
-    @Override
-    public void supprimerLigneBmq(Long ligneBmqId) {
-        ligneBmqDao.delete(trouverLigneBmq(ligneBmqId));
-    }
-    
-    @Transactional
-    @Override
-    public Bmq genererBmq(Long bmqId, Collection<Document> documents, Collection<Recouvrement> recouvrements) {
-        final Bmq bmq = trouverBmq(bmqId);
-        bmq.setLigneBmqs(genererLigneBmq(bmq, documents));
-        bmq.setRecouvrements(recouvrements);
-        bmq.setDocuments(documents);
-        return bmq;
-    }
-    
-    /**
-     * Genere les ligne du bmq à partir des documents associés.
-     * 
-     * @param bmq
-     *            Bmq concerné.
-     * @param documents
-     *            documents associés.
-     * @return
-     */
-    private Collection<LigneBmq> genererLigneBmq(Bmq bmq, Collection<Document> documents) {
-        final Collection<LigneBmq> lignes = new HashSet<>();
-        for (final Document d : documents) {
-            for (final LigneDeDocument ld : d.getLigneDocuments()) {
-                LigneBmq lb = new LigneBmq();
-                lb.setBmq(bmq);
-                lb.setDocument(d);
-                lb.setPrixUnitaireLigne(ld.getPrixunitaiteLigne());
-                lb.setProduit(ld.getProduit());
-                lb.setQuantiteLigne(ld.getQuantiteLigne());
-                lignes.add(lb);
-            }
-        }
-        return lignes;
-    }
-    
-    /**
-     * Genere la liste de taxe associee a chaque ligne de bmq
-     * 
-     * @param ligneDocumentTaxes
-     * @return
-     */
-    private Collection<LigneBmqTaxe> genererLigneBmqTaxe(Collection<LigneBmq> ligneBmqs) {
-        final Collection<LigneBmqTaxe> ligneBmqTaxes = new HashSet<>();
-        
-        for (final LigneBmq ligneBmq : ligneBmqs) {
-            
-            for (final LigneDeDocumentTaxe ligneDeDocumentTaxe : obtenirLigneDocumentTaxe(ligneBmq.getBmq())) {
-                LigneBmqTaxe ligneBmqTaxe = new LigneBmqTaxe();
-                ligneBmqTaxe.setLigneBmq(ligneBmq);
-                ligneBmqTaxe.setTaxe(ligneDeDocumentTaxe.getTaxe());
-                ligneBmqTaxe.setTauxDeTaxe(ligneDeDocumentTaxe.getTauxDeTaxe());
-                ligneBmqTaxes.add(ligneBmqTaxe);
-            }
-        }
-        return ligneBmqTaxes;
-    }
-    
-    /**
-     * Extraire la liste des taxe associe a un bmq
-     * 
-     * @param bmq
-     * @return
-     */
-    private Collection<LigneDeDocumentTaxe> obtenirLigneDocumentTaxe(Bmq bmq) {
-        final Collection<LigneDeDocumentTaxe> ligneDeDocumentTaxes = new HashSet<>();
-        
-        for (final Document d : bmq.getDocuments()) {
-            for (final LigneDeDocument ld : d.getLigneDocuments()) {
-                for (LigneDeDocumentTaxe ldt : ld.getLigneDeDocumentTaxes()) {
-                    LigneDeDocumentTaxe l = new LigneDeDocumentTaxe();
-                    l.setLigneDeDocument(ld);
-                    l.setTaxe(ldt.getTaxe());
-                    l.setTauxDeTaxe(ldt.getTauxDeTaxe());
-                    ligneDeDocumentTaxes.add(l);
-                }
-            }
-        }
-        return ligneDeDocumentTaxes;
-    }
-    
-    /**
-     * Ajouter de maniere groupe une collection de taxe a une ligne de bmq.
-     * 
-     * @param ligneBmqTaxes
-     */
-    private void ajouterLigneBmqTaxe(Collection<LigneBmq> ligneBmqs) {
-        for (final LigneBmqTaxe l : genererLigneBmqTaxe(ligneBmqs)) {
-            ajouterLigneBmqTaxe(l);
-        }
-    }
-    
-    @Transactional
-    @Override
-    public Recouvrement ajouterRecouvrement(Recouvrement recouvrement) {
-        if (recouvrement != null) {
-            recouvrementDao.save(recouvrement);
-        }
-        return recouvrement;
-    }
-    
-    @Transactional
-    @Override
-    public Recouvrement modifierRecouvrement(Recouvrement recouvrement) {
-        recouvrement.setDerniereMiseAJour(new Date());
-        recouvrementDao.saveAndFlush(recouvrement);
-        return recouvrement;
-    }
-    
-    @Override
-    public Recouvrement trouverRecouvrement(Long recouvrementId) {
-        Recouvrement r = recouvrementDao.findOne(recouvrementId);
-        if (r != null) {
-            return r;
-        } else {
-            throw new GlobalErpServiceException(
-                    "Le recouvrment ayant l'identifiant " + recouvrementId + " n'existe pas");
-        }
-    }
-    
-    @Transactional
-    @Override
-    public void supprimerRecouvrement(Long recouvrementId) {
-        recouvrementDao.delete(trouverRecouvrement(recouvrementId));
-    }
-    
-    @Transactional
-    @Override
-    public void supprimerLigneBmq(Bmq bmq) {
-        ligneBmqDao.delete(bmq.getLigneBmqs());
-    }
-    
-    @Transactional
-    @Override
-    public LigneBmqTaxe ajouterLigneBmqTaxe(LigneBmqTaxe ligneBmqTaxe) {
-        if (ligneBmqTaxe != null) {
-            ligneBmqTaxeDao.save(ligneBmqTaxe);
-        }
-        return ligneBmqTaxe;
-    }
-    
-    @Transactional
-    @Override
-    public LigneBmqTaxe modifierLigneBmqTaxe(LigneBmqTaxe ligneBmqTaxe) {
-        ligneBmqTaxe.setDerniereMiseAJour(new Date());
-        ligneBmqTaxeDao.saveAndFlush(ligneBmqTaxe);
-        return ligneBmqTaxe;
-    }
-    
-    @Override
-    public LigneBmqTaxe trouverLigneBmqTaxe(Long ligneBmqTaxeId) {
-        if (ligneBmqTaxeId == null) {
-            throw new IllegalArgumentException("ligneBmqTaxeId ne doit pas etre null");
-        }
-        LigneBmqTaxe l = ligneBmqTaxeDao.findOne(ligneBmqTaxeId);
-        if (l != null) {
-            return l;
-        } else {
-            throw new GlobalErpServiceException(GlobalAppConstants.buildNotFingMessage(LigneBmqTaxe.class, ligneBmqTaxeId));
-        }
-    }
-    
-    @Transactional
-    @Override
-    public void supprimerLigneBmqTaxe(Long ligneBmqTaxeId) {
-        ligneBmqTaxeDao.delete(trouverLigneBmqTaxe(ligneBmqTaxeId));
-    }
+
+	@Autowired
+	private BmqDao bmqDao;
+
+	@Autowired
+	private LigneBmqDao ligneBmqDao;
+
+	@Autowired
+	private RecouvrementDao recouvrementDao;
+
+	@Autowired
+	private LigneBmqTaxeDao ligneBmqTaxeDao;
+
+	@Transactional
+	@Override
+	public Bmq ajouterBmq(Bmq bmq) {
+
+		if (bmq != null) {
+			bmqDao.save(bmq);
+		}
+		return bmq;
+	}
+
+	@Transactional
+	@Override
+	public Bmq modifierBmq(Bmq bmq) {
+		bmq.setDerniereMiseAJour(new Date());
+		bmqDao.saveAndFlush(bmq);
+		return bmq;
+	}
+
+	@Override
+	public Bmq trouverBmq(Long bmqId) {
+		if (bmqId == null) {
+			throw new IllegalArgumentException("bmqId ne doit pas etre null");
+		}
+		final Bmq b = bmqDao.findOne(bmqId);
+		if (b != null) {
+			Hibernate.initialize(b.getLigneBmqs());
+			Hibernate.initialize(b.getDocuments());
+			Hibernate.initialize(b.getRecouvrements());
+			return b;
+		} else {
+			throw new GlobalErpServiceException(GlobalAppConstants.buildNotFingMessage(Bmq.class, bmqId));
+		}
+	}
+
+	@Transactional
+	@Override
+	public void supprimerBmq(Long bmqId) {
+		bmqDao.delete(trouverBmq(bmqId));
+	}
+
+	@Override
+	public Page<Bmq> listerBmq(Pageable p) {
+		return bmqDao.findAll(p);
+	}
+
+	@Override
+	public Page<Bmq> listerBmq(Long vendeurId, Pageable p) {
+		return bmqDao.listerBmq(vendeurId, p);
+	}
+
+	@Override
+	public Page<Bmq> listerBmq(Date debut, Date fin, Pageable p) {
+		return bmqDao.listerBmq(debut, fin, p);
+	}
+
+	@Override
+	public Page<Bmq> listerBmq(Long vendeurId, Date debut, Date fin, Pageable p) {
+		return bmqDao.listerBmq(vendeurId, debut, fin, p);
+	}
+
+	@Transactional
+	@Override
+	public Collection<LigneBmq> ajouterLigneBmq(Collection<LigneBmq> ligneBmqs) {
+		if (ligneBmqs != null) {
+			ligneBmqDao.save(ligneBmqs);
+			ajouterLigneBmqTaxe(ligneBmqs);
+		}
+		return ligneBmqs;
+	}
+
+	@Override
+	public LigneBmq trouverLigneBmq(Long ligneBmqId) {
+		if (ligneBmqId == null) {
+			throw new IllegalArgumentException("ligneBmqId ne doit pas etre null");
+		}
+		final LigneBmq lb = ligneBmqDao.findOne(ligneBmqId);
+		if (lb != null) {
+			lb.getLigneBmqTaxes();
+			return lb;
+		} else {
+			throw new GlobalErpServiceException(GlobalAppConstants.buildNotFingMessage(LigneBmq.class, ligneBmqId));
+		}
+	}
+
+	@Transactional
+	@Override
+	public void supprimerLigneBmq(Long ligneBmqId) {
+		ligneBmqDao.delete(trouverLigneBmq(ligneBmqId));
+	}
+
+	@Transactional
+	@Override
+	public Bmq genererBmq(Long bmqId, Collection<Document> documents, Collection<Recouvrement> recouvrements) {
+		final Bmq bmq = trouverBmq(bmqId);
+		bmq.setLigneBmqs(genererLigneBmq(bmq, documents));
+		bmq.setRecouvrements(recouvrements);
+		bmq.setDocuments(documents);
+		return bmq;
+	}
+
+	/**
+	 * Genere les ligne du bmq à partir des documents associés.
+	 * 
+	 * @param bmq
+	 *            Bmq concerné.
+	 * @param documents
+	 *            documents associés.
+	 * @return
+	 */
+	private Collection<LigneBmq> genererLigneBmq(Bmq bmq, Collection<Document> documents) {
+		final Collection<LigneBmq> lignes = new HashSet<>();
+		documents.stream().forEach(d -> {
+			d.getLigneDocuments().stream().forEach(ld -> {
+				LigneBmq lb = new LigneBmq();
+				lb.setBmq(bmq);
+				lb.setDocument(d);
+				lb.setPrixUnitaireLigne(ld.getPrixunitaiteLigne());
+				lb.setProduit(ld.getProduit());
+				lb.setQuantiteLigne(ld.getQuantiteLigne());
+				lignes.add(lb);
+			});
+		});
+		return lignes;
+	}
+
+	/**
+	 * Genere la liste de taxe associee a chaque ligne de bmq
+	 * 
+	 * @param ligneDocumentTaxes
+	 * @return
+	 */
+	private Collection<LigneBmqTaxe> genererLigneBmqTaxe(Collection<LigneBmq> ligneBmqs) {
+
+		final Collection<LigneBmqTaxe> ligneBmqTaxes = new HashSet<>();
+		ligneBmqs.stream().forEach(ligneBmq -> {
+			obtenirLigneDocumentTaxe(ligneBmq.getBmq()).stream().forEach(ligneDeDocumentTaxe -> {
+				LigneBmqTaxe ligneBmqTaxe = new LigneBmqTaxe();
+				ligneBmqTaxe.setLigneBmq(ligneBmq);
+				ligneBmqTaxe.setTaxe(ligneDeDocumentTaxe.getTaxe());
+				ligneBmqTaxe.setTauxDeTaxe(ligneDeDocumentTaxe.getTauxDeTaxe());
+				ligneBmqTaxes.add(ligneBmqTaxe);
+			});
+		});
+		return ligneBmqTaxes;
+	}
+
+	/**
+	 * Extraire la liste des taxe associe a un bmq
+	 * 
+	 * @param bmq
+	 * @return
+	 */
+	private Collection<LigneDeDocumentTaxe> obtenirLigneDocumentTaxe(Bmq bmq) {
+		final Collection<LigneDeDocumentTaxe> ligneDeDocumentTaxes = new HashSet<>();
+		bmq.getDocuments().stream().filter(d -> d.getTypeDocument().equals(TypeDocuments.DOCUMENT_DE_VENTE))
+				.forEach(d -> {
+					d.getLigneDocuments().stream().forEach(ld -> {
+						ld.getLigneDeDocumentTaxes().stream().forEach(ldt -> {
+							LigneDeDocumentTaxe l = new LigneDeDocumentTaxe();
+							l.setLigneDeDocument(ld);
+							l.setTaxe(ldt.getTaxe());
+							l.setTauxDeTaxe(ldt.getTauxDeTaxe());
+							ligneDeDocumentTaxes.add(l);
+						});
+					});
+				});
+		return ligneDeDocumentTaxes;
+	}
+
+	/**
+	 * Ajouter de maniere groupe une collection de taxe a une ligne de bmq.
+	 * 
+	 * @param ligneBmqTaxes
+	 */
+	private void ajouterLigneBmqTaxe(Collection<LigneBmq> ligneBmqs) {
+		genererLigneBmqTaxe(ligneBmqs).stream().forEach(l -> {
+			ajouterLigneBmqTaxe(l);
+		});
+	}
+
+	@Transactional
+	@Override
+	public Recouvrement ajouterRecouvrement(Recouvrement recouvrement) {
+		if (recouvrement != null) {
+			recouvrementDao.save(recouvrement);
+		}
+		return recouvrement;
+	}
+
+	@Transactional
+	@Override
+	public Recouvrement modifierRecouvrement(Recouvrement recouvrement) {
+		recouvrement.setDerniereMiseAJour(new Date());
+		recouvrementDao.saveAndFlush(recouvrement);
+		return recouvrement;
+	}
+
+	@Override
+	public Recouvrement trouverRecouvrement(Long recouvrementId) {
+		Recouvrement r = recouvrementDao.findOne(recouvrementId);
+		if (r != null) {
+			return r;
+		} else {
+			throw new GlobalErpServiceException(
+					"Le recouvrment ayant l'identifiant " + recouvrementId + " n'existe pas");
+		}
+	}
+
+	@Transactional
+	@Override
+	public void supprimerRecouvrement(Long recouvrementId) {
+		recouvrementDao.delete(trouverRecouvrement(recouvrementId));
+	}
+
+	@Transactional
+	@Override
+	public void supprimerLigneBmq(Bmq bmq) {
+		ligneBmqDao.delete(bmq.getLigneBmqs());
+	}
+
+	@Transactional
+	@Override
+	public LigneBmqTaxe ajouterLigneBmqTaxe(LigneBmqTaxe ligneBmqTaxe) {
+		if (ligneBmqTaxe != null) {
+			ligneBmqTaxeDao.save(ligneBmqTaxe);
+		}
+		return ligneBmqTaxe;
+	}
+
+	@Transactional
+	@Override
+	public LigneBmqTaxe modifierLigneBmqTaxe(LigneBmqTaxe ligneBmqTaxe) {
+		ligneBmqTaxe.setDerniereMiseAJour(new Date());
+		ligneBmqTaxeDao.saveAndFlush(ligneBmqTaxe);
+		return ligneBmqTaxe;
+	}
+
+	@Override
+	public LigneBmqTaxe trouverLigneBmqTaxe(Long ligneBmqTaxeId) {
+		if (ligneBmqTaxeId == null) {
+			throw new IllegalArgumentException("ligneBmqTaxeId ne doit pas etre null");
+		}
+		LigneBmqTaxe l = ligneBmqTaxeDao.findOne(ligneBmqTaxeId);
+		if (l != null) {
+			return l;
+		} else {
+			throw new GlobalErpServiceException(
+					GlobalAppConstants.buildNotFingMessage(LigneBmqTaxe.class, ligneBmqTaxeId));
+		}
+	}
+
+	@Transactional
+	@Override
+	public void supprimerLigneBmqTaxe(Long ligneBmqTaxeId) {
+		ligneBmqTaxeDao.delete(trouverLigneBmqTaxe(ligneBmqTaxeId));
+	}
 }
--- src/main/java/com/camlait/global/erp/service/document/DocumentService.java
+++ src/main/java/com/camlait/global/erp/service/document/DocumentService.java
@@ -18,178 +18,176 @@ import com.camlait.global.erp.domain.document.Document;
 import com.camlait.global.erp.domain.document.LigneDeDocument;
 import com.camlait.global.erp.domain.document.LigneDeDocumentTaxe;
 import com.camlait.global.erp.domain.document.commerciaux.vente.FactureClient;
-import com.camlait.global.erp.domain.produit.ProduitTaxe;
 import com.camlait.global.erp.service.GlobalErpServiceException;
 
 public class DocumentService implements IDocumentService {
-    
-    @Autowired
-    private DocumentDao documentDao;
-    
-    @Autowired
-    private LigneDocumentDao ligneDeDocumentDao;
-    
-    @Autowired
-    private LigneDeDocumentTaxeDao ligneDeDocumentTaxeDao;
-    
-    @Transactional
-    @Override
-    public Document ajouterDocument(Document document) {
-        if (document != null) {
-            documentDao.save(document);
-        }
-        return document;
-    }
-    
-    @Transactional
-    @Override
-    public Document modifierDocument(Document document) {
-        document.setDerniereMiseAJour(new Date());
-        documentDao.saveAndFlush(document);
-        return document;
-    }
-    
-    @Override
-    public Document trouverDocument(Long documentId) {
-        if (documentId == null) {
-            throw new IllegalArgumentException("documentId ne doit pas etre null");
-        }
-        Document d = documentDao.findOne(documentId);
-        if (d != null) {
-            Hibernate.initialize(d.getLigneDocuments());
-            if (d instanceof FactureClient) {
-                Hibernate.initialize(((FactureClient) d).getFactureReglements());
-            }
-            return d;
-        } else {
-            throw new GlobalErpServiceException(GlobalAppConstants.buildNotFingMessage(Document.class, documentId));
-        }
-    }
-    
-    @Transactional
-    @Override
-    public void supprimerDocument(Long documentId) {
-        documentDao.delete(trouverDocument(documentId));
-    }
-    
-    @Override
-    public Page<Document> listerDocument(Date debut, Date fin, Pageable p) {
-        return documentDao.listerDocument(debut, fin, p);
-    }
-    
-    @Transactional
-    @Override
-    public LigneDeDocument ajouterLigneDocument(LigneDeDocument ligne) {
-        if (ligne != null) {
-            ligneDeDocumentDao.save(ligne);
-            ajouterLigneDeDocumentTaxe(ligne);
-        }
-        return ligne;
-    }
-    
-    @Transactional
-    @Override
-    public Collection<LigneDeDocument> ajouterLigneDocument(Collection<LigneDeDocument> lignes) {
-        if (lignes != null) {
-            ligneDeDocumentDao.save(lignes);
-            ajouterLigneDeDocumentTaxe(lignes);
-        }
-        return lignes;
-    }
-    
-    @Transactional
-    @Override
-    public LigneDeDocument modifierLigneDocument(LigneDeDocument ligne) {
-        ligne.setDerniereMiseAJour(new Date());
-        ligneDeDocumentDao.saveAndFlush(ligne);
-        return ligne;
-    }
-    
-    @Override
-    public LigneDeDocument trouverLigneDocument(Long ligneId) {
-        if (ligneId == null) {
-            throw new IllegalArgumentException("ligneId ne doit pas etre null");
-        }
-        LigneDeDocument ld = ligneDeDocumentDao.findOne(ligneId);
-        if (ld != null) {
-            Hibernate.initialize(ld.getLigneDeDocumentTaxes());
-            return ld;
-        } else {
-            throw new GlobalErpServiceException(GlobalAppConstants.buildNotFingMessage(LigneDeDocument.class, ligneId));
-        }
-    }
-    
-    @Transactional
-    @Override
-    public void supprimerLigneDocument(Long ligneId) {
-        ligneDeDocumentDao.delete(trouverLigneDocument(ligneId));
-    }
-    
-    @Transactional
-    @Override
-    public void supprimerLigneDocument(Document document) {
-        ligneDeDocumentDao.delete(document.getLigneDocuments());
-    }
-    
-    @Transactional
-    @Override
-    public LigneDeDocumentTaxe ajouterLigneDeDocumentTaxe(LigneDeDocumentTaxe ligneDeDocumentTaxe) {
-        if (ligneDeDocumentTaxe != null) {
-            ligneDeDocumentTaxeDao.save(ligneDeDocumentTaxe);
-        }
-        return ligneDeDocumentTaxe;
-    }
-    
-    @Transactional
-    @Override
-    public LigneDeDocumentTaxe modifierLigneDeDocumentTaxe(LigneDeDocumentTaxe ligneDeDocumentTaxe) {
-        ligneDeDocumentTaxe.setDerniereMiseAJour(new Date());
-        ligneDeDocumentTaxeDao.saveAndFlush(ligneDeDocumentTaxe);
-        return ligneDeDocumentTaxe;
-    }
-    
-    @Override
-    public LigneDeDocumentTaxe trouverLigneDeDocumentTaxe(Long ligneDeDocumentTaxeId) {
-        if (ligneDeDocumentTaxeId == null) {
-            throw new IllegalArgumentException("ligneDeDocumentTaxeId ne peut pas etre null");
-        }
-        LigneDeDocumentTaxe l = ligneDeDocumentTaxeDao.findOne(ligneDeDocumentTaxeId);
-        if (l != null) {
-            return l;
-        } else {
-            throw new GlobalErpServiceException(GlobalAppConstants.buildNotFingMessage(LigneDeDocumentTaxe.class, ligneDeDocumentTaxeId));
-        }
-    }
-    
-    @Transactional
-    @Override
-    public void spprimerLigneDeDocumentTaxe(Long ligneDeDocumentTaxeId) {
-        ligneDeDocumentTaxeDao.delete(trouverLigneDeDocumentTaxe(ligneDeDocumentTaxeId));
-    }
-    
-    /**
-     * Ajout des taxe a une ligne de document.
-     * 
-     * @param ligneDeDocument
-     */
-    private void ajouterLigneDeDocumentTaxe(LigneDeDocument ligneDeDocument) {
-        for (ProduitTaxe pt : ligneDeDocument.getProduit().getProduitTaxes()) {
-            LigneDeDocumentTaxe l = new LigneDeDocumentTaxe();
-            l.setLigneDeDocument(ligneDeDocument);
-            l.setTaxe(pt.getTaxe());
-            l.setTauxDeTaxe(pt.getTaxe().getValeurPourcentage());
-            ajouterLigneDeDocumentTaxe(l);
-        }
-    }
-    
-    /**
-     * Ajout des taxes aux lignes de document de maniere groupee.
-     * 
-     * @param lignes
-     */
-    private void ajouterLigneDeDocumentTaxe(Collection<LigneDeDocument> lignes) {
-        for (LigneDeDocument ligne : lignes) {
-            ajouterLigneDeDocumentTaxe(ligne);
-        }
-    }
+
+	@Autowired
+	private DocumentDao documentDao;
+
+	@Autowired
+	private LigneDocumentDao ligneDeDocumentDao;
+
+	@Autowired
+	private LigneDeDocumentTaxeDao ligneDeDocumentTaxeDao;
+
+	@Transactional
+	@Override
+	public Document ajouterDocument(Document document) {
+		if (document != null) {
+			documentDao.save(document);
+		}
+		return document;
+	}
+
+	@Transactional
+	@Override
+	public Document modifierDocument(Document document) {
+		document.setDerniereMiseAJour(new Date());
+		documentDao.saveAndFlush(document);
+		return document;
+	}
+
+	@Override
+	public Document trouverDocument(Long documentId) {
+		if (documentId == null) {
+			throw new IllegalArgumentException("documentId ne doit pas etre null");
+		}
+		Document d = documentDao.findOne(documentId);
+		if (d != null) {
+			Hibernate.initialize(d.getLigneDocuments());
+			if (d instanceof FactureClient) {
+				Hibernate.initialize(((FactureClient) d).getFactureReglements());
+			}
+			return d;
+		} else {
+			throw new GlobalErpServiceException(GlobalAppConstants.buildNotFingMessage(Document.class, documentId));
+		}
+	}
+
+	@Transactional
+	@Override
+	public void supprimerDocument(Long documentId) {
+		documentDao.delete(trouverDocument(documentId));
+	}
+
+	@Override
+	public Page<Document> listerDocument(Date debut, Date fin, Pageable p) {
+		return documentDao.listerDocument(debut, fin, p);
+	}
+
+	@Transactional
+	@Override
+	public LigneDeDocument ajouterLigneDocument(LigneDeDocument ligne) {
+		if (ligne != null) {
+			ligneDeDocumentDao.save(ligne);
+			ajouterLigneDeDocumentTaxe(ligne);
+		}
+		return ligne;
+	}
+
+	@Transactional
+	@Override
+	public Collection<LigneDeDocument> ajouterLigneDocument(Collection<LigneDeDocument> lignes) {
+		if (lignes != null) {
+			ligneDeDocumentDao.save(lignes);
+			ajouterLigneDeDocumentTaxe(lignes);
+		}
+		return lignes;
+	}
+
+	@Transactional
+	@Override
+	public LigneDeDocument modifierLigneDocument(LigneDeDocument ligne) {
+		ligne.setDerniereMiseAJour(new Date());
+		ligneDeDocumentDao.saveAndFlush(ligne);
+		return ligne;
+	}
+
+	@Override
+	public LigneDeDocument trouverLigneDocument(Long ligneId) {
+		if (ligneId == null) {
+			throw new IllegalArgumentException("ligneId ne doit pas etre null");
+		}
+		LigneDeDocument ld = ligneDeDocumentDao.findOne(ligneId);
+		if (ld != null) {
+			Hibernate.initialize(ld.getLigneDeDocumentTaxes());
+			return ld;
+		} else {
+			throw new GlobalErpServiceException(GlobalAppConstants.buildNotFingMessage(LigneDeDocument.class, ligneId));
+		}
+	}
+
+	@Transactional
+	@Override
+	public void supprimerLigneDocument(Long ligneId) {
+		ligneDeDocumentDao.delete(trouverLigneDocument(ligneId));
+	}
+
+	@Transactional
+	@Override
+	public void supprimerLigneDocument(Document document) {
+		ligneDeDocumentDao.delete(document.getLigneDocuments());
+	}
+
+	@Transactional
+	@Override
+	public LigneDeDocumentTaxe ajouterLigneDeDocumentTaxe(LigneDeDocumentTaxe ligneDeDocumentTaxe) {
+		if (ligneDeDocumentTaxe != null) {
+			ligneDeDocumentTaxeDao.save(ligneDeDocumentTaxe);
+		}
+		return ligneDeDocumentTaxe;
+	}
+
+	@Transactional
+	@Override
+	public LigneDeDocumentTaxe modifierLigneDeDocumentTaxe(LigneDeDocumentTaxe ligneDeDocumentTaxe) {
+		ligneDeDocumentTaxe.setDerniereMiseAJour(new Date());
+		ligneDeDocumentTaxeDao.saveAndFlush(ligneDeDocumentTaxe);
+		return ligneDeDocumentTaxe;
+	}
+
+	@Override
+	public LigneDeDocumentTaxe trouverLigneDeDocumentTaxe(Long ligneDeDocumentTaxeId) {
+		if (ligneDeDocumentTaxeId == null) {
+			throw new IllegalArgumentException("ligneDeDocumentTaxeId ne peut pas etre null");
+		}
+		LigneDeDocumentTaxe l = ligneDeDocumentTaxeDao.findOne(ligneDeDocumentTaxeId);
+		if (l != null) {
+			return l;
+		} else {
+			throw new GlobalErpServiceException(
+					GlobalAppConstants.buildNotFingMessage(LigneDeDocumentTaxe.class, ligneDeDocumentTaxeId));
+		}
+	}
+
+	@Transactional
+	@Override
+	public void spprimerLigneDeDocumentTaxe(Long ligneDeDocumentTaxeId) {
+		ligneDeDocumentTaxeDao.delete(trouverLigneDeDocumentTaxe(ligneDeDocumentTaxeId));
+	}
+
+	/**
+	 * Ajout des taxe a une ligne de document.
+	 * 
+	 * @param ligneDeDocument
+	 */
+	private void ajouterLigneDeDocumentTaxe(LigneDeDocument ligneDeDocument) {
+		ligneDeDocument.getProduit().getProduitTaxes().stream().forEach(pt -> {
+			LigneDeDocumentTaxe l = new LigneDeDocumentTaxe();
+			l.setLigneDeDocument(ligneDeDocument);
+			l.setTaxe(pt.getTaxe());
+			l.setTauxDeTaxe(pt.getTaxe().getValeurPourcentage());
+			ajouterLigneDeDocumentTaxe(l);
+		});
+	}
+
+	/**
+	 * Ajout des taxes aux lignes de document de maniere groupee.
+	 * 
+	 * @param lignes
+	 */
+	private void ajouterLigneDeDocumentTaxe(Collection<LigneDeDocument> lignes) {
+		lignes.stream().forEach(ligne -> ajouterLigneDeDocumentTaxe(ligne));
+	}
 }
