93c843ca6e0ff20c9b9b7009627e5d4d86a15a4a
martin <a-msigne@expedia.com>
2016-01-15 16:47:23 -0500

    Code refactoring

--- src/main/java/com/camlait/global/erp/service/auth/AuthentificationService.java
+++ src/main/java/com/camlait/global/erp/service/auth/AuthentificationService.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
 
@@ -34,248 +35,258 @@ import static com.camlait.global.erp.domain.config.GlobalAppConstants.*;
 
 @Transactional
 public class AuthentificationService implements IAuthentificationService {
-
-	@Autowired
-	private UtilisateurDao utilisateurDao;
-
-	@Autowired
-	private RessourceDao ressourceDao;
-
-	@Autowired
-	private RessourceUtilisateurDao ressourceUtilisateurDao;
-
-	@Autowired
-	private LanguageDao langueDao;
-
-	@Autowired
-	private TermeDao termeDao;
-
-	@Autowired
-	private TermeLangueDao termeLangueDao;
-
-	@Override
-	public Utilisateur ajouterUtilisateur(Utilisateur utilisateur) {
-		verifyIllegalArgumentException(utilisateur, "utilisateur");
-		utilisateurDao.save(utilisateur);
-		return utilisateur;
-	}
-
-	@Override
-	public Utilisateur modifierUtilisateur(Utilisateur utilisateur) {
-		verifyIllegalArgumentException(utilisateur, "utilisateur");
-		utilisateur.setDerniereMiseAJour(new Date());
-		utilisateurDao.saveAndFlush(utilisateur);
-		return utilisateur;
-	}
-
-	@Override
-	public Utilisateur obtenirUtilisateur(String codeUtilisateur) {
-		verifyIllegalArgumentException(codeUtilisateur, "codeUtilisateur");
-		final Utilisateur u = utilisateurDao.findOne(codeUtilisateur);
-		verifyObjectNoFindException(u, Utilisateur.class, codeUtilisateur);
-		Hibernate.initialize(u.getEmployes());
-		// Hibernate.initialize(u.getRessourceUtilsateurs());
-		// Hibernate.initialize(u.getGroupeUtilisateurs());
-		return u;
-	}
-
-	@Override
-	public Page<Utilisateur> obtenirUtilisateurParCourriel(String courriel, Pageable p) {
-		return utilisateurDao.findUtilisateurByCourriel(courriel, p);
-	}
-
-	@Override
-	public Page<Utilisateur> listerUtilisateur(String motCle, Pageable p) {
-		return utilisateurDao.listerUtilisateur(motCle, p);
-	}
-
-	@Override
-	public Page<Utilisateur> listerUtilisateur(Pageable p) {
-		return utilisateurDao.findAll(p);
-	}
-
-	@Override
-	public void supprimerUtilisateur(String codeUtilisateur) {
-		utilisateurDao.delete(obtenirUtilisateur(codeUtilisateur));
-	}
-
-	@Override
-	public Ressource ajouterRessource(Ressource ressource) throws GlobalErpServiceException, IllegalArgumentException {
-		verifyIllegalArgumentException(ressource, "ressource");
-		ressourceDao.save(ressource);
-		return ressource;
-	}
-
-	@Override
-	public Ressource modifierRessource(Ressource ressource) throws GlobalErpServiceException, IllegalArgumentException {
-		verifyIllegalArgumentException(ressource, "ressource");
-		ressourceDao.saveAndFlush(ressource);
-		return ressource;
-	}
-
-	@Override
-	public Ressource obtenirRessource(Long ressourceId) throws GlobalErpServiceException, IllegalArgumentException {
-		verifyIllegalArgumentException(ressourceId, "ressourceId");
-		final Ressource r = ressourceDao.findOne(ressourceId);
-		verifyObjectNoFindException(r, Ressource.class, ressourceId);
-		r.getRessourceFilles();
-		return r;
-	}
-
-	@Override
-	public Ressource obtenirRessource(String codeRessource) throws GlobalErpServiceException, IllegalArgumentException {
-		verifyIllegalArgumentException(codeRessource, "codeRessource");
-		final List<Ressource> ressources = ressourceDao.findByCodeRessource(codeRessource, new PageRequest(0, 1))
-				.getContent();
-		final Ressource r = (ressources.isEmpty()) ? null : ressources.get(0);
-		verifyObjectNoFindException(r, Ressource.class, codeRessource);
-		return r;
-
-	}
-
-	@Override
-	public void supprimerRessource(Long ressourceId) throws GlobalErpServiceException, IllegalArgumentException {
-		verifyIllegalArgumentException(ressourceId, "ressourceId");
-		ressourceDao.delete(obtenirRessource(ressourceId));
-	}
-
-	@Override
-	public RessourceUtilisateur ajouterRessourceUtilisateur(RessourceUtilisateur ressourceUtilisateur)
-			throws GlobalErpServiceException, IllegalArgumentException {
-		verifyIllegalArgumentException(ressourceUtilisateur, "ressourceUtilisateur");
-		ressourceUtilisateurDao.save(ressourceUtilisateur);
-		return ressourceUtilisateur;
-	}
-
-	@Override
-	public RessourceUtilisateur modifierRessourceUtilisateur(RessourceUtilisateur ressourceUtilisateur)
-			throws GlobalErpServiceException, IllegalArgumentException {
-		verifyIllegalArgumentException(ressourceUtilisateur, "ressourceUtilisateur");
-		ressourceUtilisateurDao.saveAndFlush(ressourceUtilisateur);
-		return ressourceUtilisateur;
-	}
-
-	@Override
-	public RessourceUtilisateur obtenirRessourceUtilisateur(Long ressourceUtilisateurId)
-			throws GlobalErpServiceException, IllegalArgumentException {
-		verifyIllegalArgumentException(ressourceUtilisateurId, "ressourceUtilisateurId");
-		RessourceUtilisateur ru = ressourceUtilisateurDao.findOne(ressourceUtilisateurId);
-		verifyObjectNoFindException(ru, RessourceUtilisateur.class, ressourceUtilisateurId);
-		return ru;
-	}
-
-	@Override
-	public void supprimerRessourceUtilisateur(Long ressourceUtilisateurId)
-			throws GlobalErpServiceException, IllegalArgumentException {
-		verifyIllegalArgumentException(ressourceUtilisateurId, "ressourceUtilisateurId");
-		ressourceUtilisateurDao.delete(obtenirRessourceUtilisateur(ressourceUtilisateurId));
-	}
-
-	@Override
-	public Langue ajouterLangue(Langue langue) throws GlobalErpServiceException, IllegalArgumentException {
-		langueDao.save(langue);
-		return langue;
-	}
-
-	@Override
-	public Langue modifierLangue(Langue langue) throws GlobalErpServiceException, IllegalArgumentException {
-		langueDao.saveAndFlush(langue);
-		return langue;
-	}
-
-	@Override
-	public Langue obtenirLangue(Long langueId) throws GlobalErpServiceException, IllegalArgumentException {
-		Langue l = langueDao.findOne(langueId);
-		if (l != null) {
-			Hibernate.initialize(l.getTermeLangues());
-		}
-		return l;
-	}
-
-	@Override
-	public Langue obtenirLangue(String codeLangue) throws GlobalErpServiceException, IllegalArgumentException {
-		return langueDao.findByCodeLangue(codeLangue);
-	}
-
-	@Override
-	public void supprimerLangue(Long langueId) throws GlobalErpServiceException, IllegalArgumentException {
-		langueDao.delete(obtenirLangue(langueId));
-	}
-
-	@Override
-	public Collection<LangueModel> listerLangue() {
-		Collection<LangueModel> lms = new HashSet<>();
-		langueDao.findAll().stream().forEach(l -> {
-			lms.add(new LangueModel(l));
-		});
-		return lms;
-	}
-
-	@Override
-	public Terme ajouterTerme(Terme terme) {
-		termeDao.save(terme);
-		return terme;
-	}
-
-	@Override
-	public Terme modifierTerme(Terme terme) {
-		termeDao.saveAndFlush(terme);
-		return terme;
-	}
-
-	@Override
-	public Terme obtenirTerme(Long termeId) {
-		return termeDao.findOne(termeId);
-	}
-
-	@Override
-	public Terme obtenirTerme(String descriptionTerme) {
-		return termeDao.findByDescriptionTerme(descriptionTerme);
-	}
-
-	@Override
-	public void supprimerTerme(Long termeId) {
-		termeDao.delete(obtenirTerme(termeId));
-	}
-
-	@Override
-	public Page<Terme> listerTerme(Pageable p) {
-		return termeDao.findAll(p);
-	}
-
-	@Override
-	public TermeLangue ajouterTermeLangue(TermeLangue termeLangue) {
-		termeLangueDao.save(termeLangue);
-		return termeLangue;
-	}
-
-	@Override
-	public TermeLangue modifierTermeLangue(TermeLangue termeLangue) {
-		termeLangueDao.saveAndFlush(termeLangue);
-		return termeLangue;
-	}
-
-	@Override
-	public TermeLangue obtenirTermeLangue(Long termeLangueId) {
-		return termeLangueDao.findOne(termeLangueId);
-	}
-
-	@Override
-	public void supprimerTermeLangue(Long termeLangueId) {
-		termeLangueDao.delete(obtenirTermeLangue(termeLangueId));
-	}
-
-	@Override
-	public Map<String, String> listerTerme(Long langueId) {
-		Map<String, String> termes = new HashMap<>();
-		obtenirLangue(langueId).getTermeLangues().stream().forEach(tl -> {
-			termes.put(tl.getTerme().getDescriptionTerme(), tl.getValue());
-		});
-		return termes;
-	}
-
-	@Override
-	public Map<String, String> listerTerme(String codeLangue) {
-		return listerTerme(obtenirLangue(codeLangue).getLangId());
-	}
+    
+    @Autowired
+    private UtilisateurDao utilisateurDao;
+    
+    @Autowired
+    private RessourceDao ressourceDao;
+    
+    @Autowired
+    private RessourceUtilisateurDao ressourceUtilisateurDao;
+    
+    @Autowired
+    private LanguageDao langueDao;
+    
+    @Autowired
+    private TermeDao termeDao;
+    
+    @Autowired
+    private TermeLangueDao termeLangueDao;
+    
+    @Override
+    public Utilisateur ajouterUtilisateur(Utilisateur utilisateur) {
+        verifyIllegalArgumentException(utilisateur, "utilisateur");
+        utilisateurDao.save(utilisateur);
+        return utilisateur;
+    }
+    
+    @Override
+    public Utilisateur modifierUtilisateur(Utilisateur utilisateur) {
+        verifyIllegalArgumentException(utilisateur, "utilisateur");
+        utilisateur.setDerniereMiseAJour(new Date());
+        utilisateurDao.saveAndFlush(utilisateur);
+        return utilisateur;
+    }
+    
+    @Override
+    public Utilisateur obtenirUtilisateur(String codeUtilisateur) {
+        verifyIllegalArgumentException(codeUtilisateur, "codeUtilisateur");
+        final Utilisateur u = utilisateurDao.findOne(codeUtilisateur);
+        verifyObjectNoFindException(u, Utilisateur.class, codeUtilisateur);
+        Hibernate.initialize(u.getEmployes());
+        // Hibernate.initialize(u.getRessourceUtilsateurs());
+        // Hibernate.initialize(u.getGroupeUtilisateurs());
+        return u;
+    }
+    
+    @Override
+    public Page<Utilisateur> obtenirUtilisateurParCourriel(String courriel, Pageable p) {
+        return utilisateurDao.findUtilisateurByCourriel(courriel, p);
+    }
+    
+    @Override
+    public Page<Utilisateur> listerUtilisateur(String motCle, Pageable p) {
+        return utilisateurDao.listerUtilisateur(motCle, p);
+    }
+    
+    @Override
+    public Page<Utilisateur> listerUtilisateur(Pageable p) {
+        return utilisateurDao.findAll(p);
+    }
+    
+    @Override
+    public void supprimerUtilisateur(String codeUtilisateur) {
+        utilisateurDao.delete(obtenirUtilisateur(codeUtilisateur));
+    }
+    
+    @Override
+    public Ressource ajouterRessource(Ressource ressource) throws GlobalErpServiceException, IllegalArgumentException {
+        verifyIllegalArgumentException(ressource, "ressource");
+        ressourceDao.save(ressource);
+        return ressource;
+    }
+    
+    @Override
+    public Ressource modifierRessource(Ressource ressource) throws GlobalErpServiceException, IllegalArgumentException {
+        verifyIllegalArgumentException(ressource, "ressource");
+        ressourceDao.saveAndFlush(ressource);
+        return ressource;
+    }
+    
+    @Override
+    public Ressource obtenirRessource(Long ressourceId) throws GlobalErpServiceException, IllegalArgumentException {
+        verifyIllegalArgumentException(ressourceId, "ressourceId");
+        final Ressource r = ressourceDao.findOne(ressourceId);
+        verifyObjectNoFindException(r, Ressource.class, ressourceId);
+        r.getRessourceFilles();
+        return r;
+    }
+    
+    @Override
+    public Ressource obtenirRessource(String codeRessource) throws GlobalErpServiceException, IllegalArgumentException {
+        verifyIllegalArgumentException(codeRessource, "codeRessource");
+        final List<Ressource> ressources = ressourceDao.findByCodeRessource(codeRessource, new PageRequest(0, 1))
+                .getContent();
+        final Ressource r = (ressources.isEmpty()) ? null : ressources.get(0);
+        verifyObjectNoFindException(r, Ressource.class, codeRessource);
+        return r;
+        
+    }
+    
+    @Override
+    public void supprimerRessource(Long ressourceId) throws GlobalErpServiceException, IllegalArgumentException {
+        verifyIllegalArgumentException(ressourceId, "ressourceId");
+        ressourceDao.delete(obtenirRessource(ressourceId));
+    }
+    
+    @Override
+    public RessourceUtilisateur ajouterRessourceUtilisateur(RessourceUtilisateur ressourceUtilisateur)
+            throws GlobalErpServiceException, IllegalArgumentException {
+        verifyIllegalArgumentException(ressourceUtilisateur, "ressourceUtilisateur");
+        ressourceUtilisateurDao.save(ressourceUtilisateur);
+        return ressourceUtilisateur;
+    }
+    
+    @Override
+    public RessourceUtilisateur modifierRessourceUtilisateur(RessourceUtilisateur ressourceUtilisateur)
+            throws GlobalErpServiceException, IllegalArgumentException {
+        verifyIllegalArgumentException(ressourceUtilisateur, "ressourceUtilisateur");
+        ressourceUtilisateurDao.saveAndFlush(ressourceUtilisateur);
+        return ressourceUtilisateur;
+    }
+    
+    @Override
+    public RessourceUtilisateur obtenirRessourceUtilisateur(Long ressourceUtilisateurId)
+            throws GlobalErpServiceException, IllegalArgumentException {
+        verifyIllegalArgumentException(ressourceUtilisateurId, "ressourceUtilisateurId");
+        RessourceUtilisateur ru = ressourceUtilisateurDao.findOne(ressourceUtilisateurId);
+        verifyObjectNoFindException(ru, RessourceUtilisateur.class, ressourceUtilisateurId);
+        return ru;
+    }
+    
+    @Override
+    public void supprimerRessourceUtilisateur(Long ressourceUtilisateurId)
+            throws GlobalErpServiceException, IllegalArgumentException {
+        verifyIllegalArgumentException(ressourceUtilisateurId, "ressourceUtilisateurId");
+        ressourceUtilisateurDao.delete(obtenirRessourceUtilisateur(ressourceUtilisateurId));
+    }
+    
+    @Override
+    public Langue ajouterLangue(Langue langue) throws GlobalErpServiceException, IllegalArgumentException {
+        langueDao.save(langue);
+        return langue;
+    }
+    
+    @Override
+    public Langue modifierLangue(Langue langue) throws GlobalErpServiceException, IllegalArgumentException {
+        langueDao.saveAndFlush(langue);
+        return langue;
+    }
+    
+    @Override
+    public Langue obtenirLangue(Long langueId) throws GlobalErpServiceException, IllegalArgumentException {
+        Langue l = langueDao.findOne(langueId);
+        if (l != null) {
+            Hibernate.initialize(l.getTermeLangues());
+        }
+        return l;
+    }
+    
+    @Override
+    public Langue obtenirLangue(String codeLangue) throws GlobalErpServiceException, IllegalArgumentException {
+        return langueDao.findByCodeLangue(codeLangue);
+    }
+    
+    @Override
+    public void supprimerLangue(Long langueId) throws GlobalErpServiceException, IllegalArgumentException {
+        langueDao.delete(obtenirLangue(langueId));
+    }
+    
+    @Override
+    public Collection<LangueModel> listerLangue() {
+        Collection<LangueModel> lms = new HashSet<>();
+        langueDao.findAll().stream().forEach(l -> {
+            lms.add(new LangueModel(l));
+        });
+        return lms;
+    }
+    
+    @Override
+    public Terme ajouterTerme(Terme terme) {
+        termeDao.save(terme);
+        return terme;
+    }
+    
+    @Override
+    public Terme modifierTerme(Terme terme) {
+        termeDao.saveAndFlush(terme);
+        return terme;
+    }
+    
+    @Override
+    public Terme obtenirTerme(Long termeId) {
+        return termeDao.findOne(termeId);
+    }
+    
+    @Override
+    public Terme obtenirTerme(String descriptionTerme) {
+        return termeDao.findByDescriptionTerme(descriptionTerme);
+    }
+    
+    @Override
+    public void supprimerTerme(Long termeId) {
+        termeDao.delete(obtenirTerme(termeId));
+    }
+    
+    @Override
+    public Page<Terme> listerTerme(Pageable p) {
+        return termeDao.findAll(p);
+    }
+    
+    @Override
+    public TermeLangue ajouterTermeLangue(TermeLangue termeLangue) {
+        termeLangueDao.save(termeLangue);
+        return termeLangue;
+    }
+    
+    @Override
+    public TermeLangue modifierTermeLangue(TermeLangue termeLangue) {
+        termeLangueDao.saveAndFlush(termeLangue);
+        return termeLangue;
+    }
+    
+    @Override
+    public TermeLangue obtenirTermeLangue(Long termeLangueId) {
+        return termeLangueDao.findOne(termeLangueId);
+    }
+    
+    @Override
+    public void supprimerTermeLangue(Long termeLangueId) {
+        termeLangueDao.delete(obtenirTermeLangue(termeLangueId));
+    }
+    
+    @Override
+    public Map<String, String> listerTerme(Long langueId) {
+        Map<String, String> termes = new HashMap<>();
+        obtenirLangue(langueId).getTermeLangues().stream().forEach(tl -> {
+            termes.put(tl.getTerme().getDescriptionTerme(), tl.getValue());
+        });
+        return termes;
+    }
+    
+    @Override
+    public Map<String, String> listerTerme(String codeLangue) {
+        return listerTerme(obtenirLangue(codeLangue).getLangId());
+    }
+    
+    @Override
+    public boolean termeNonCharge(String descriptionTerme, Langue l) {
+        return l.getTermeLangues().stream()
+                .filter(tl -> tl.getTerme()
+                        .getDescriptionTerme()
+                        .equalsIgnoreCase(descriptionTerme))
+                .collect(Collectors.toList()).isEmpty();
+                
+    }
 }
--- src/main/java/com/camlait/global/erp/service/auth/IAuthentificationService.java
+++ src/main/java/com/camlait/global/erp/service/auth/IAuthentificationService.java
@@ -16,77 +16,79 @@ import com.camlait.global.erp.domain.exception.GlobalErpServiceException;
 import com.camlait.global.erp.domain.model.json.auth.LangueModel;
 
 public interface IAuthentificationService {
-
-	Utilisateur ajouterUtilisateur(Utilisateur utilisateur) throws GlobalErpServiceException, IllegalArgumentException;
-
-	Utilisateur modifierUtilisateur(Utilisateur utilisateur) throws GlobalErpServiceException, IllegalArgumentException;
-
-	Utilisateur obtenirUtilisateur(String codeUtilisateur) throws GlobalErpServiceException;
-
-	Page<Utilisateur> obtenirUtilisateurParCourriel(String courriel, Pageable p)
-			throws GlobalErpServiceException, IllegalArgumentException;
-
-	Page<Utilisateur> listerUtilisateur(String motCle, Pageable p) throws GlobalErpServiceException;
-
-	Page<Utilisateur> listerUtilisateur(Pageable p) throws GlobalErpServiceException, IllegalArgumentException;
-
-	void supprimerUtilisateur(String codeUtilisateur) throws GlobalErpServiceException, IllegalArgumentException;
-
-	Ressource ajouterRessource(Ressource ressource) throws GlobalErpServiceException, IllegalArgumentException;
-
-	Ressource modifierRessource(Ressource ressource) throws GlobalErpServiceException, IllegalArgumentException;
-
-	Ressource obtenirRessource(Long ressourceId) throws GlobalErpServiceException, IllegalArgumentException;
-
-	Ressource obtenirRessource(String codeRessource) throws GlobalErpServiceException, IllegalArgumentException;
-
-	void supprimerRessource(Long ressourceId) throws GlobalErpServiceException, IllegalArgumentException;
-
-	RessourceUtilisateur ajouterRessourceUtilisateur(RessourceUtilisateur ressourceUtilisateur)
-			throws GlobalErpServiceException, IllegalArgumentException;
-
-	RessourceUtilisateur modifierRessourceUtilisateur(RessourceUtilisateur ressourceUtilisateur)
-			throws GlobalErpServiceException, IllegalArgumentException;
-
-	RessourceUtilisateur obtenirRessourceUtilisateur(Long ressourceUtilisateurId)
-			throws GlobalErpServiceException, IllegalArgumentException;
-
-	void supprimerRessourceUtilisateur(Long ressourceUtilisateurId)
-			throws GlobalErpServiceException, IllegalArgumentException;
-
-	Langue ajouterLangue(Langue langue) throws GlobalErpServiceException, IllegalArgumentException;
-
-	Langue modifierLangue(Langue langue) throws GlobalErpServiceException, IllegalArgumentException;
-
-	Langue obtenirLangue(Long langueId) throws GlobalErpServiceException, IllegalArgumentException;
-
-	Langue obtenirLangue(String codeLangue) throws GlobalErpServiceException, IllegalArgumentException;
-
-	void supprimerLangue(Long langueId) throws GlobalErpServiceException, IllegalArgumentException;
-
-	Collection<LangueModel> listerLangue();
-
-	Terme ajouterTerme(Terme terme);
-
-	Terme modifierTerme(Terme terme);
-
-	Terme obtenirTerme(Long termeId);
-
-	Terme obtenirTerme(String descriptionTerme);
-
-	void supprimerTerme(Long termeId);
-
-	Page<Terme> listerTerme(Pageable p);
-
-	Map<String, String> listerTerme(Long langueId);
-
-	Map<String, String> listerTerme(String codeLangue);
-
-	TermeLangue ajouterTermeLangue(TermeLangue termeLangue);
-
-	TermeLangue modifierTermeLangue(TermeLangue termeLangue);
-
-	TermeLangue obtenirTermeLangue(Long termeLangueId);
-
-	void supprimerTermeLangue(Long termeLangueId);
+    
+    Utilisateur ajouterUtilisateur(Utilisateur utilisateur) throws GlobalErpServiceException, IllegalArgumentException;
+    
+    Utilisateur modifierUtilisateur(Utilisateur utilisateur) throws GlobalErpServiceException, IllegalArgumentException;
+    
+    Utilisateur obtenirUtilisateur(String codeUtilisateur) throws GlobalErpServiceException;
+    
+    Page<Utilisateur> obtenirUtilisateurParCourriel(String courriel, Pageable p)
+            throws GlobalErpServiceException, IllegalArgumentException;
+            
+    Page<Utilisateur> listerUtilisateur(String motCle, Pageable p) throws GlobalErpServiceException;
+    
+    Page<Utilisateur> listerUtilisateur(Pageable p) throws GlobalErpServiceException, IllegalArgumentException;
+    
+    void supprimerUtilisateur(String codeUtilisateur) throws GlobalErpServiceException, IllegalArgumentException;
+    
+    Ressource ajouterRessource(Ressource ressource) throws GlobalErpServiceException, IllegalArgumentException;
+    
+    Ressource modifierRessource(Ressource ressource) throws GlobalErpServiceException, IllegalArgumentException;
+    
+    Ressource obtenirRessource(Long ressourceId) throws GlobalErpServiceException, IllegalArgumentException;
+    
+    Ressource obtenirRessource(String codeRessource) throws GlobalErpServiceException, IllegalArgumentException;
+    
+    void supprimerRessource(Long ressourceId) throws GlobalErpServiceException, IllegalArgumentException;
+    
+    RessourceUtilisateur ajouterRessourceUtilisateur(RessourceUtilisateur ressourceUtilisateur)
+            throws GlobalErpServiceException, IllegalArgumentException;
+            
+    RessourceUtilisateur modifierRessourceUtilisateur(RessourceUtilisateur ressourceUtilisateur)
+            throws GlobalErpServiceException, IllegalArgumentException;
+            
+    RessourceUtilisateur obtenirRessourceUtilisateur(Long ressourceUtilisateurId)
+            throws GlobalErpServiceException, IllegalArgumentException;
+            
+    void supprimerRessourceUtilisateur(Long ressourceUtilisateurId)
+            throws GlobalErpServiceException, IllegalArgumentException;
+            
+    Langue ajouterLangue(Langue langue) throws GlobalErpServiceException, IllegalArgumentException;
+    
+    Langue modifierLangue(Langue langue) throws GlobalErpServiceException, IllegalArgumentException;
+    
+    Langue obtenirLangue(Long langueId) throws GlobalErpServiceException, IllegalArgumentException;
+    
+    Langue obtenirLangue(String codeLangue) throws GlobalErpServiceException, IllegalArgumentException;
+    
+    void supprimerLangue(Long langueId) throws GlobalErpServiceException, IllegalArgumentException;
+    
+    Collection<LangueModel> listerLangue();
+    
+    Terme ajouterTerme(Terme terme);
+    
+    Terme modifierTerme(Terme terme);
+    
+    Terme obtenirTerme(Long termeId);
+    
+    Terme obtenirTerme(String descriptionTerme);
+    
+    void supprimerTerme(Long termeId);
+    
+    Page<Terme> listerTerme(Pageable p);
+    
+    Map<String, String> listerTerme(Long langueId);
+    
+    Map<String, String> listerTerme(String codeLangue);
+    
+    TermeLangue ajouterTermeLangue(TermeLangue termeLangue);
+    
+    TermeLangue modifierTermeLangue(TermeLangue termeLangue);
+    
+    TermeLangue obtenirTermeLangue(Long termeLangueId);
+    
+    void supprimerTermeLangue(Long termeLangueId);
+    
+    boolean termeNonCharge(String descriptionTerme, Langue langue);
 }
